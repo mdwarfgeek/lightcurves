6e958c2c7e78eaaab83b7c6f4d687b5be1aa6ffc
jmi <>
2014-10-09 18:23:53 +0000

    Moved cplate to lib.

--- xytoxy.c
+++ xytoxy.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 #include <math.h>
 
 #include "lightcurves.h"
@@ -8,113 +9,6 @@
 
 #define NITER 5
 #define NSIG  3
-
-static void cplate (struct lc_point *combuf, struct lc_star *refbuf, double *wtbuf,
-		    long npt, double tr[6]) {
-  double xmatx[50][50], vect[50];
-
-  double sx1sq = 0.0, sx1 = 0.0, sy1sq = 0.0, sy1 = 0.0;
-  double sx1y1 = 0.0, sx1x2 = 0.0, sy1x2 = 0.0;
-  double sx2 = 0.0, sy1y2 = 0.0, sx1y2 = 0.0, sy2 = 0.0;
-  double xbar = 0.0, ybar = 0.0, xref = 0.0, yref = 0.0, sw = 0.0;
-  double wt, xx1, yy1, xx2, yy2;
-
-  long i;
-
-  /* find <x> and <y> for comparison and reference files */
-  for(i = 0; i < npt; i++) {
-    wt = (wtbuf ? wtbuf[i] : 1.0);
-
-    xbar += combuf[i].x * wt;
-    ybar += combuf[i].y * wt;
-    xref += refbuf[i].x * wt;
-    yref += refbuf[i].y * wt;
-    sw += wt;
-  }
-
-  xbar /= sw;
-  ybar /= sw;
-  xref /= sw;
-  yref /= sw;
-
-  /* accumulate sums */
-  sw = 0;
-
-  for(i = 0; i < npt; i++) {
-    wt = (wtbuf ? wtbuf[i] : 1.0);
-
-    xx1 = combuf[i].x-xbar;           /* zero-mean for more stable solution */
-    xx2 = refbuf[i].x-xref;
-    yy1 = combuf[i].y-ybar;
-    yy2 = refbuf[i].y-yref;
-    sx1sq += xx1*xx1 * wt;
-    sx1 += xx1 * wt;
-    sy1sq += yy1*yy1 * wt;
-    sy1 += yy1 * wt;
-    sx1y1 += xx1*yy1 * wt;
-    sx1x2 += xx1*xx2 * wt;
-    sy1x2 += yy1*xx2 * wt;
-    sx2 += xx2 * wt;
-    sy1y2 += yy1*yy2 * wt;
-    sx1y2 += xx1*yy2 * wt;
-    sy2 += yy2 * wt;
-    sw += wt;
-  }
-
-  /* fill in matrix-vector for a b c */
-  xmatx[0][0] = sx1sq;
-  xmatx[1][0] = sx1y1;
-  xmatx[2][0] = sx1;
-  xmatx[0][1] = sx1y1;
-  xmatx[1][1] = sy1sq;
-  xmatx[2][1] = sy1;
-  xmatx[0][2] = sx1;
-  xmatx[1][2] = sy1;
-  xmatx[2][2] = sw;
-  vect[0] = sx1x2;
-  vect[1] = sy1x2;
-  vect[2] = sx2;
-
-  dsolve(xmatx, vect, 3);
-
-  tr[0] = vect[0];
-  tr[1] = vect[1];
-  tr[2] = vect[2] - xbar*tr[0] - ybar*tr[1] + xref;    /* put back in original system */
-
-  if(vect[0] == 0 && vect[1] == 0 && vect[2] == 0) {
-    /* Failed */
-    tr[0] = 1.0;
-    tr[1] = 0.0;
-    tr[2] = xbar - xref;
-  }
-
-  /* fill in matrix-vector for d e f */
-  xmatx[0][0] = sy1sq;
-  xmatx[1][0] = sx1y1;
-  xmatx[2][0] = sy1;
-  xmatx[0][1] = sx1y1;
-  xmatx[1][1] = sx1sq;
-  xmatx[2][1] = sx1;
-  xmatx[0][2] = sy1;
-  xmatx[1][2] = sx1;
-  xmatx[2][2] = sw;
-  vect[0] = sy1y2;
-  vect[1] = sx1y2;
-  vect[2] = sy2;
-  
-  dsolve(xmatx, vect, 3);
-
-  tr[3] = vect[0];
-  tr[4] = vect[1];
-  tr[5] = vect[2] - xbar*tr[4] - ybar*tr[3] + yref;
-
-  if(vect[0] == 0 && vect[1] == 0 && vect[2] == 0) {
-    /* Failed */
-    tr[3] = 1.0;
-    tr[4] = 0.0;
-    tr[5] = ybar - yref;
-  }
-}
 
 int xytoxy (struct lc_point *data, struct lc_mef *mefinfo,
 	    double tr[6], char *errstr) {
@@ -174,8 +68,10 @@ int xytoxy (struct lc_point *data, struct lc_mef *mefinfo,
     if(iter > 0) {
       for(star = 0; star < mefinfo->nstars; star++)
 	if(!rejbuf[star]) {
-	  dx = tr[0]*data[star].x+tr[1]*data[star].y+tr[2] - mefinfo->stars[star].x;
-	  dy = tr[3]*data[star].y+tr[4]*data[star].x+tr[5] - mefinfo->stars[star].y;
+	  dx = dplate_tr_x(data[star].x, data[star].y, tr)
+             - mefinfo->stars[star].x;
+          dy = dplate_tr_y(data[star].x, data[star].y, tr)
+             - mefinfo->stars[star].y;
 	  err = sqrt(dx*dx+dy*dx);
 	
 	  if(err > NSIG * averr) {
@@ -192,8 +88,13 @@ int xytoxy (struct lc_point *data, struct lc_mef *mefinfo,
 	  nuse++;
     }    
 
-    if(nuse > 6)
-      cplate(data, mefinfo->stars, wtbuf, mefinfo->nstars, tr);
+    if(nuse >= 3)
+      dplate(data, offsetof(struct lc_point, x), sizeof(struct lc_point),
+             data, offsetof(struct lc_point, y), sizeof(struct lc_point),
+             mefinfo->stars, offsetof(struct lc_star, x), sizeof(struct lc_star),
+             mefinfo->stars, offsetof(struct lc_star, y), sizeof(struct lc_star),
+             wtbuf, 0, sizeof(double),
+             mefinfo->nstars, 6, tr);
     else
       break;
 
@@ -203,8 +104,10 @@ int xytoxy (struct lc_point *data, struct lc_mef *mefinfo,
       if(rejbuf[star])
 	nrej++;
       else {
-	dx = tr[0]*data[star].x+tr[1]*data[star].y+tr[2] - mefinfo->stars[star].x;
-	dy = tr[3]*data[star].y+tr[4]*data[star].x+tr[5] - mefinfo->stars[star].y;
+        dx = dplate_tr_x(data[star].x, data[star].y, tr)
+           - mefinfo->stars[star].x;
+        dy = dplate_tr_y(data[star].x, data[star].y, tr)
+           - mefinfo->stars[star].y;
 	
 	tmpbufx[nuse] = fabsf(dx);
 	tmpbufy[nuse] = fabsf(dy);
